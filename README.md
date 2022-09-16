4621ef58f0d41e9af240dc4afe2725ede593f05c
Zhanlong Yang <yangzhanlong@xinyu668.com>
2022-09-16 10:33:02 +0800

    删除无用资源

---
+++ README.md
@@ -0,0 +1,1 @@
+* **[AndroidGuide 在线阅读网站](https://yangzhanlong.github.io/)**
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ import theme from "./theme.js";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "AndroidGuide",
-  description: "「Android学习之路」一份涵盖大部分 Android 程序员所需要掌握的核心知识。",
+  description: "「Android学习之路」一份涵盖大部分 Android 程序员所需要掌握的核心知识(包括：Java， FrameWork，Kotlin， 音视频， Flutter等)。",
   //指定 vuepress build 的输出目录
   dest: "./dist",
   // 是否开启默认预加载 js
