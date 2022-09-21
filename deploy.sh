0f2416a51490b855b00308942a576d29d6c45cfd
Zhanlong Yang <yangzhanlong@xinyu668.com>
2022-09-21 10:00:03 +0800

    refine

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,9 @@ set -e
 npm run build 
 # 进入生成的文件夹
 cd dist
-# 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-# 如果你想要部署到 https://USERNAME.github.io
 git push -f https://github.com/yangzhanlong/yangzhanlong.github.io.git master
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 cd -
 
